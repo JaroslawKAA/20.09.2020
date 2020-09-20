0398b9d1c756659f0b1c544819627195cb936fde
JaroslawKAA <kaszczak.jaroslaw@gmail.com>
2020-09-20 10:56:03 +0200

    Update 2

--- parser.py
+++ parser.py
@@ -1,6 +1,33 @@
 import sys
 
 
+NAME_INDEX = 0
+
+def generate_raport(data_list):
+    output = []
+
+    output.append(f"Ilość danych: {len(data_list)}.")
+    output.append(f"\nImiona: {get_names(data_list)}")
+
+    return output
+
+
+def get_names(data_list):
+    names = []
+
+    for line in data_list:
+        names.append(line[NAME_INDEX])
+
+    return ", ".join(names)
+
+
+def save_raport_to_file(raport, filename):
+    with open(filename, "w+") as file_to_read:
+        for raport_data in raport:
+            file_to_read.write(raport_data)
+
+
+
 def get_filename():
     if len(sys.argv) >= 2:
         filename = sys.argv[1]
@@ -27,6 +54,9 @@ def main():
     print(f"File opened: {filename}")
     data_list = read_file_to_list(filename)
     print(data_list)
+    raport = generate_raport(data_list)
+    print(raport)
+    save_raport_to_file(raport, "raport.txt")
 
 if __name__ == "__main__":
     main()
